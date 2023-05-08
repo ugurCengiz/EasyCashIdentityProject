766b89fce8c8e90cbd616dc3bb9059144d1d5406
uğur cengiz <ugur_nobre@hotmail.com>
2023-05-08 15:10:42 +0300

    08.05.23

---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T>:IGenericDal<T> where T:class
+    {
+        public void Insert(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+           return context.Set<T>().ToList();
+
+        }
+    }
+}
